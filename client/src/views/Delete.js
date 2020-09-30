ae25475d91e4d1d635c6fc3c6dd0d8b76beb89e4
cox260 <cox260@purdue.edu>
2020-09-29 20:57:22 -0400

    Added delete button

--- client/src/views/Delete.js
+++ client/src/views/Delete.js
@@ -3,12 +3,11 @@ import '../css/app.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default class Settings extends React.Component {  
+export default class Delete extends React.Component {  
     render() {
         return (
             <div>
                 <Header />
-                <h2><Link to="delete" class="button">Delete Account</Link></h2>
                 <Footer />
             </div>
         )
