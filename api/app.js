0afe0e8bcc70b39a0aa4fd85233695ba359ec379
Andrew <smit3407@purdue.edu>
2020-10-01 15:16:37 -0400

    Added MySQL support

--- api/app.js
+++ api/app.js
@@ -1,73 +1,26 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// mySQL and TDS (Tedious)
-var Connection = require("tedious").Connection;
-var Request = require("tedious").Request;
+const express = require("express");
+const path = require("path");
 
-var cors = require("cors");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+const apiRouter = require("./routes/api");
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(cors());
-
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// mount our api router here
+app.use("/api", apiRouter);
 
-// mySQL
-// example: https://docs.microsoft.com/en-us/sql/connect/node-js/step-3-proof-of-concept-connecting-to-sql-using-node-js?view=sql-server-ver15
-var Connection = require('tedious').Connection;  
-var config = {  
-  server: 'localhost',  //update me
-  authentication: {
-    type: 'default',
-    options: {
-      userName: 'admin', //update me
-      password: 'admin5',  //update me
-      validateBulkLoadParameters: 'false'
-    }
-  },
-  options: {
-  // If you are on Microsoft Azure, you need encryption:
-      encrypt: true,
-      database: 'Database1'  //update me
-  }
-};
-var connection = new Connection(config);  
-connection.on('connect', function(err) {  
-  // If no error, then good to proceed.
-  console.log("Connected");  
-});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.use("/testAPI", testAPIRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  console.log("req.path", req.path);
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
 module.exports = app;
--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from '../components/Logo'
 import '../css/app.css';
 
 export default class Login extends React.Component {  
     render() {
         return (
-            <div>
-            
-                <h2><Link to="create" class="button">Create Account</Link></h2>
+            <div class='middle'>
+                <Logo/>
+                <div class="login-form">
+                    <form action="auth" method="POST">
+                        <input type="text" name="username" placeholder="Username" required/>
+                        <input type="password" name="password" placeholder="Password" required/>
+                        <input type="submit" placeholer="Log In"/>
+                    </form>
+                </div>
 
                 <h2><Link to="feed" class="button">Log In</Link></h2>
-
+                <h4>Not a member?</h4>
+                <h2><Link to="create" class="button">Create Account</Link></h2>
             </div>
         )
     }
