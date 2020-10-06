29713d1b4c6996eca97345858f371fac0d1c7543
cox260 <cox260@purdue.edu>
2020-10-05 22:35:48 -0400

    added logout features I believe

--- client/src/views/LogOut.js
+++ client/src/views/LogOut.js
@@ -1,17 +1,128 @@
 import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles'
+import Logo from '../components/Logo'
 import '../css/app.css';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
-export default class Feed extends React.Component {  
-    render() {
-        return (
-            <div>
-                <Header />
-                <h2><Link to="Feed" class="button">Confirm Log Out</Link></h2>
-                <Footer />
-            </div>
-        )
+//MUI Stuff
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = (theme) => ({
+    ...theme.spreadIt
+})
+
+class LogOut extends React.Component {  
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {}
+        }
     }
+    
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            loading: true
+        });
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        axios
+            .post('/logout', userData)
+            .then(res => {
+                console.log(res.data)
+                localStorage.setItem('FBIdToken', `Bearer  ${res.data.token}`);
+                this.setState({
+                    loading: false
+                });
+                this.props.history.push('/home');
+            })
+            .catch(err => {
+                this.setState({
+                    errors: err.response.data,
+                    loading: false
+                })
+            })
+    }
+
+    render() {
+        const { classes } = this.props;
+        const { errors, loading } = this.state;
+
+        return ( 
+            <div className='start'>
+                <Grid container className={classes.form} direction='column'>
+                    <Grid item sm />
+                    <Grid item sm>
+                        <div className='middle'>
+                        <Logo />
+                        <Typography variant='h2' className={classes.pageTitle} >
+                            Account Log Out
+                        </Typography>
+                        <form noValidate onSubmit={this.handleSubmit}>
+                            <TextField 
+                                id="email" 
+                                name="email" 
+                                type="email" 
+                                label="Email" 
+                                className={classes.textField}
+                                helperText={errors.email} 
+                                error={errors.email ? true : false} 
+                                value={this.state.email} 
+                                onChange={this.handleChange} 
+                                fullwidth />
+                            <br />
+                            <TextField 
+                                id="password" 
+                                name="password" 
+                                type="password" 
+                                label="Password" 
+                                className={classes.textField}
+                                helperText={errors.password} 
+                                error={errors.password ? true : false} 
+                                value={this.state.password} 
+                                onChange={this.handleChange} 
+                                fullwidth />
+                            <br />
+                            { errors.general && (
+                                <Typography variant='h2' className={classes.customError}>
+                                    {errors.general}
+                                </Typography>
+                            )}
+                            <Button type="submit" variant="contained" color="primary" className={classes.Button} disable={loading}>
+                                Log in
+                                {loading && (
+                                    <CircularProgress size={20} className={classes.progress}/>
+                                )}
+                            </Button>
+                            <br />
+                        </form>
+                        </div>
+                    </Grid>
+                    <Grid item sm />
+                </Grid>
+            </div>
+    )}
 }
+
+LogOut.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(LogOut);
