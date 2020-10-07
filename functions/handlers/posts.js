f8f14af177288012c2a45d1cbc387a992f2309e3
Gaurav Manglani <gmanglan@purdue.edu>
2020-10-07 13:20:31 -0400

    Removed extra anonymous post method

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -31,7 +31,7 @@ exports.createPost = (req, res) => {
       body: req.body,
       userHandle: req.userHandle,
       createdAt: new Date().toISOString(),
-      isAnonymous: false
+      isAnonymous: req.isAnonymous
     }
     db.settings({ ignoreUndefinedProperties: true })
     db
@@ -43,27 +43,4 @@ exports.createPost = (req, res) => {
       .catch((err) => {
         res.status(500).json({ error: 'something went wrong' });
     });
-}
-
-exports.creatAnonymousPost = (req, res) => {
-  if(req.body === '') {
-    return res.status(400).json({body: 'Body must not be empty'});
-  }
-
-  const newPost = {
-    body: req.body,
-    userHandle: req.userHandle,
-    createdAt: new Date().toISOString(),
-    isAnonymous: true
-  }
-  db.settings({ ignoreUndefinedProperties: true })
-  db
-    .collection('posts')
-    .add(newPost)
-    .then((doc) => {
-      res.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: 'something went wrong' });
-  });
 }
