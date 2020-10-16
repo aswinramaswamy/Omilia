f3478d30cce5e5e46a533c80430a35124be8478b
Andrew <smit3407@purdue.edu>
2020-10-16 09:12:51 -0400

    Starting redux integration

--- client/src/index.js
+++ client/src/index.js
@@ -1,70 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as  Router, Route, Switch } from 'react-router-dom';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import themeFile from './css/theme';
-import jwtDecode from 'jwt-decode';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./css/theme";
+import jwtDecode from "jwt-decode";
+
+//Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 //Import Component
-import AuthRoute from './components/AuthRoute'
+import AuthRoute from "./components/AuthRoute";
 
 //Import Views
-import Create from './views/Create';
-import Home from './views/Home';
-import Login from './views/Login';
-import LogOut from './views/LogOut';
-import Delete from './views/Delete';
-import NotFound from './views/NotFound';
-import Settings from './views/Settings';
-import Start from './views/Start';
-import Profile from './views/Profile';
-import ChangeEmail from './views/ChangeEmail';
-import ChangePassword from './views/ChangePassword';
+import Create from "./views/Create";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import LogOut from "./views/LogOut";
+import Delete from "./views/Delete";
+import NotFound from "./views/NotFound";
+import Settings from "./views/Settings";
+import Start from "./views/Start";
+import Profile from "./views/Profile";
+import ChangeEmail from "./views/ChangeEmail";
+import ChangePassword from "./views/ChangePassword";
 
-import './css/app.css'
-import FullPagePost from './views/FullPagePost';
-import NewPost from './views/NewPost';
-import DeleteAccount from './views/DeleteAccount';
+import "./css/app.css";
+import FullPagePost from "./views/FullPagePost";
+import NewPost from "./views/NewPost";
+import DeleteAccount from "./views/DeleteAccount";
 
 const theme = createMuiTheme(themeFile);
 
 const token = localStorage.FBIdToken;
 
 let authenticated;
-if(token){
-    const decodedToken = jwtDecode(token);
-    if (decodedToken.exp * 1000 < Date.now()) {
-        window.location.href='/login'
-        authenticated=false;
-    } else {
-        authenticated=true;
-    }
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
 }
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
-        <Router>
-            <div>
-            <Switch>
-                <AuthRoute exact path="/" component={Start} authenticated={authenticated}/>
-                <Route path="/home" component={Home} />
-                <AuthRoute path="/create" component={Create} authenticated={authenticated}/>
-                <AuthRoute path="/login" component={Login} authenticated={authenticated}/>
-                <Route path="/logout" component={LogOut} />
-                <Route path="/settings" component={Settings} />
-                <Route path="/deleteAccount" component={DeleteAccount} />
-                <Route path="/profile" component={Profile} />
-                <Route path="/post" component={FullPagePost} /> 
-                <Route path="/post/:postid" component={FullPagePost} />
-                <Route path="/ChangeEmail" component={ChangeEmail} />
-                <Route path="/ChangePassword" component={ChangePassword} />
-                <Route path="/NewPost" component={NewPost} />
-                {/* Only useful in development mode */}
-                <Route component={NotFound} status={404} />
-            </Switch>
-            </div>
-        </Router>
-    </MuiThemeProvider>,
-document.getElementById('root'),
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Switch>
+            <AuthRoute
+              exact
+              path="/"
+              component={Start}
+              authenticated={authenticated}
+            />
+            <Route path="/home" component={Home} />
+            <AuthRoute
+              path="/create"
+              component={Create}
+              authenticated={authenticated}
+            />
+            <AuthRoute
+              path="/login"
+              component={Login}
+              authenticated={authenticated}
+            />
+            <Route path="/logout" component={LogOut} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/deleteAccount" component={DeleteAccount} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/post" component={FullPagePost} />
+            <Route path="/post/:postid" component={FullPagePost} />
+            <Route path="/ChangeEmail" component={ChangeEmail} />
+            <Route path="/ChangePassword" component={ChangePassword} />
+            <Route path="/NewPost" component={NewPost} />
+            {/* Only useful in development mode */}
+            <Route component={NotFound} status={404} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById("root")
 );
--- client/src/views/LogOut.js
+++ client/src/views/LogOut.js
@@ -1,132 +1,147 @@
-import React from 'react';
-import withStyles from '@material-ui/core/styles/withStyles'
-import Logo from '../components/Logo'
-import '../css/app.css';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Logo from "../components/Logo";
+import "../css/app.css";
+import PropTypes from "prop-types";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 //MUI Stuff
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 //import IconButton from '@material-ui/core/IconButton'
 //import { logoutUser } from '../redux/actions/userActions'
 
 const styles = (theme) => ({
-    ...theme.spreadIt
-})
+  ...theme.spreadIt,
+});
 
-class LogOut extends React.Component {  
-    constructor(){
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            loading: false,
-            errors: {}
-        }
-    }
-    
-    handleChange = (event) => {
+class LogOut extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleLogOut = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/logout", userData)
+      .then((res) => {
+        console.log(res.data);
+        localStorage.removeItem("FBIdToken", `Bearer ${res.data.token}`);
         this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-    
-    handleLogOut = (event) => {
-        event.preventDefault();
-        this.setState({
-            loading: true
+          loading: false,
         });
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        }
-        axios
-            .post('/logout', userData)
-            .then(res => {
-                console.log(res.data)
-                localStorage.removeItem('FBIdToken', `Bearer ${res.data.token}`);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/');
-            })
-            .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
-                })
-            })
-    }
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
 
-    render() {
-        const { classes } = this.props;
-        const { errors, loading } = this.state;
+  render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
 
-        return ( 
-            <div className='start'>
-                <Grid container className={classes.form} direction='column'>
-                    <Grid item sm />
-                    <Grid item sm>
-                        <div className='middle'>
-                        <Logo />
-                        <Typography variant='h2' className={classes.pageTitle} >
-                            Account Log Out
-                        </Typography>
-                        <form noValidate onSubmit={this.handleSubmit}>
-                            <TextField 
-                                id="email" 
-                                name="email" 
-                                type="email" 
-                                label="Email" 
-                                className={classes.textField}
-                                helperText={errors.email} 
-                                error={errors.email ? true : false} 
-                                value={this.state.email} 
-                                onChange={this.handleChange} 
-                                fullwidth />
-                            <br />
-                            <TextField 
-                                id="password" 
-                                name="password" 
-                                type="password" 
-                                label="Password" 
-                                className={classes.textField}
-                                helperText={errors.password} 
-                                error={errors.password ? true : false} 
-                                value={this.state.password} 
-                                onChange={this.handleChange} 
-                                fullwidth />
-                            <br />
-                            { errors.general && (
-                                <Typography variant='h2' className={classes.customError}>
-                                    {errors.general}
-                                </Typography>
-                            )}
-                            <Link to ="/"><Button type="submit" variant="contained" color="primary" className={classes.Button} disable={loading}>
-                                Log Out
-                                {loading && (
-                                    <CircularProgress size={20} className={classes.progress}/>
-                                )}
-                            </Button></Link>
-                            <br />
-                            <small>Don't want to log out? Cancel <Link to ="/home">here</Link>.</small>
-                        </form>
-                        </div>
-                    </Grid>
-                    <Grid item sm />
-                </Grid>
+    return (
+      <div className="start">
+        <Grid container className={classes.form} direction="column">
+          <Grid item sm />
+          <Grid item sm>
+            <div className="middle">
+              <Logo />
+              <Typography variant="h2" className={classes.pageTitle}>
+                Account Log Out
+              </Typography>
+              <form noValidate onSubmit={this.handleSubmit}>
+                <TextField
+                  id="email"
+                  name="email"
+                  type="email"
+                  label="Email"
+                  className={classes.textField}
+                  helperText={errors.email}
+                  error={errors.email ? true : false}
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  fullwidth
+                />
+                <br />
+                <TextField
+                  id="password"
+                  name="password"
+                  type="password"
+                  label="Password"
+                  className={classes.textField}
+                  helperText={errors.password}
+                  error={errors.password ? true : false}
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  fullwidth
+                />
+                <br />
+                {errors.general && (
+                  <Typography variant="h2" className={classes.customError}>
+                    {errors.general}
+                  </Typography>
+                )}
+                <Link to="/">
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    className={classes.Button}
+                    disable={loading}
+                  >
+                    Log Out
+                    {loading && (
+                      <CircularProgress
+                        size={20}
+                        className={classes.progress}
+                      />
+                    )}
+                  </Button>
+                </Link>
+                <br />
+                <small>
+                  Don't want to log out? Cancel <Link to="/home">here</Link>.
+                </small>
+              </form>
             </div>
-        )
-    }
+          </Grid>
+          <Grid item sm />
+        </Grid>
+      </div>
+    );
+  }
 }
 
 LogOut.propTypes = {
-    classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(LogOut);
