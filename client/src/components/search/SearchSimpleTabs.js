1d082f64c1670abaa8e02d8ce578ebe2ede5ae06
Gaurav Manglani <gmanglan@purdue.edu>
2020-11-25 22:27:55 -0500

    updated import for Post.js

--- client/src/components/search/SearchSimpleTabs.js
+++ client/src/components/search/SearchSimpleTabs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import User from '../layout/UserDisplay';
-import Post from '../layout/Post';
+import Post from '../layout/Post/Post';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
