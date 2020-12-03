14742aecaebcecc4e523e2ff2c328dcd10be175e
Andrew <smit3407@purdue.edu>
2020-12-03 15:53:07 -0500

    Fixed the weird error

--- client/src/components/layout/Post/Post.js
+++ client/src/components/layout/Post/Post.js
@@ -22,7 +22,6 @@ import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 import ShareIcon from '@material-ui/icons/Share';
-import Typography from '@material-ui/core/Typography'
 
 //const logUsername = localStorage.getItem('username');
 
@@ -194,7 +193,7 @@ class Post extends Component {
             </IconButton>
           );
 
-          const shareButton = this.likedPost() ? (
+          /*const shareButton = this.likedPost() ? (
             <IconButton tip="share" onClick={this.unlikePost}>
               <ShareIcon color="secondary" />
             </IconButton>
@@ -202,7 +201,7 @@ class Post extends Component {
             <IconButton tip="dislike" onClick={this.likePost}>
               <ShareIcon color="secondary" />
             </IconButton>
-          );
+          );*/
 
           const dislikeButton = this.dislikedPost() ? (
             <IconButton tip="Undo dislike" onClick={this.undislikePost}>
@@ -255,7 +254,7 @@ class Post extends Component {
                     {likeButton}
                     {dislikeButton}
                     {saveButton}
-                    {shareButton}
+                    {/*shareButton*/}
                     <PostDialog postID={postID} userHandle={userHandle}/>
                     <Typography>{realCommentCount}</Typography>
                 </CardActions>
